b09d18009a69f8603cc483f398993b4daad433bb
jrvillalba1993 <62047235+jrvillalba1993@users.noreply.github.com>
2020-06-25 02:33:23 -0400

    JS added to index

---
+++ index.js
@@ -0,0 +1,75 @@
+var inquirer = require("inquirer");
+var Letter = require("./Letter");
+var Word = require("./Word");
+
+var acceptedKeys = "abcdefghijklmnopqrstuvwxyz";
+var guessesAllowed = 10;
+var currentUnguessedCount;
+var winCount = 0;
+var lossCount = 0;
+var message;
+
+var wordOptions = ["aardvark", "wandering", "succesful", "house", "knot", "show", "saunter", "useless"];
+
+function newGame() {
+    currentUnguessedCount = 1;
+    var randomPick = Math.floor(Math.random() * wordOptions.length);
+    var randomWord = wordOptions[randomPick];
+    var newWord = new Word(randomWord, [], 10);
+    newWord.parseLetters();
+    newWord.guessString();
+    function userInput() {
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    name: "guess",
+                    message: "Guess a letter!",
+                    validate: function (input) {
+                        if (acceptedKeys.indexOf(input) !== -1 && input.length === 1) {
+                            return true;
+                        } else {
+                            console.log("\nNot a valid character.. Please use a single, lowercase letter");
+                            return false;
+                        }
+                    }
+                }
+            ]).then(function (answers) {
+                currentUnguessedCount = 0;
+                for (i = 0; i < newWord.letters.length; i++) {
+                    switch (newWord.letters[i].letter) {
+                        case (answers.guess):
+                            newWord.letters[i].guess(answers.guess);
+                            break;
+                    }; //Marks letters.guessed as true when correctly guessed
+                    switch (newWord.letters[i].guessed) {
+                        case false:
+                            currentUnguessedCount++;
+                    };
+                };
+                newWord.guessString();
+                switch (currentUnguessedCount) {
+                    case 0:
+                        winCount++;
+                        console.log("Congratulations! You've won!\nSo far, you've won " + winCount + " games!\n----------------------------\nHere's your next word!");
+                        newGame();
+                        break;
+                    default:
+                        switch (newWord.guessesRemaining) {
+                            case 0:
+                                console.log("You've used all your guesses! Try a new word.");
+                                newGame();
+                                break;
+                            default:
+                                newWord.wrongGuess(answers.guess);
+                                userInput();
+                        };
+
+                };
+            });
+
+    };
+    userInput();
+}
+console.log("Welcome! Choose any lowercase letter A-Z to guess your first word!")
+newGame();
